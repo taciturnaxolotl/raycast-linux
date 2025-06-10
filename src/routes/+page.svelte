b3390d1aa3b35002eb719e53f52761abe7e46bc7
ByteAtATime <byteatatime@proton.me>
2025-06-10 11:47:23 -0700

    feat: add input

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  import { Input } from "$lib/components/ui/input";
+
   async function run() {
     const { runPlugin } = await import("$lib");
 
@@ -6,4 +8,9 @@
   }
 </script>
 
-<button onclick={run}>Run Plugin</button>
+<div class="flex flex-col items-center h-screen">
+  <Input
+    class="rounded-none border-0 border-b focus-visible:border-b-foreground transition-colors duration-75"
+  />
+  <button onclick={run}>Run Plugin</button>
+</div>
--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 import { sveltekit } from "@sveltejs/kit/vite";
 
 // @ts-expect-error process is a nodejs global
@@ -6,7 +7,7 @@ const host = process.env.TAURI_DEV_HOST;
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
-  plugins: [sveltekit()],
+  plugins: [tailwindcss(), sveltekit()],
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
