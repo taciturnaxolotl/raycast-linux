3938a2d395e98fedc96b5c481f8411be7c3d1e11
ByteAtATime <byteatatime@proton.me>
2025-06-10 12:48:09 -0700

    feat: enhance toast component with dynamic styling based on toast type

--- src/lib/components/Toast.svelte
+++ src/lib/components/Toast.svelte
@@ -4,6 +4,17 @@
   import { Toast } from "$lib/api/toast";
 
   const toast = getToast();
+
+  const styles = $derived.by(() => {
+    switch (toast?.style) {
+      case Toast.Style.Success:
+        return "bg-green-500/20";
+      case Toast.Style.Failure:
+        return "bg-red-500/20";
+      default:
+        return "bg-gray-500/20";
+    }
+  });
 </script>
 
 <Separator />
@@ -11,7 +22,15 @@
 {#if toast}
   {@const actualToast = toast as Toast}
 
-  <div class="py-2 px-4">
+  <div class="py-2 px-4 flex items-center gap-4 {styles}">
+    {#if actualToast.style === "SUCCESS"}
+      <div class="w-2.5 h-2.5 bg-green-500 rounded-full"></div>
+    {:else if actualToast.style === "FAILURE"}
+      <div class="w-2.5 h-2.5 bg-red-500 rounded-full"></div>
+    {:else if actualToast.style === "ANIMATED"}
+      <div class="w-2.5 h-2.5 bg-gray-500 rounded-full"></div>
+    {/if}
+
     {actualToast.title}
   </div>
 {/if}
