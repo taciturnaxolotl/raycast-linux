7a873b5f8ffabed464b0f35ffbf8e09e818e1cb8
ByteAtATime <byteatatime@proton.me>
2025-06-14 14:31:37 -0700

    refactor: centralize node component mapping in nodes.ts to simplify NodeRenderer logic

--- src/lib/components/NodeRenderer.svelte
+++ src/lib/components/NodeRenderer.svelte
@@ -1,23 +1,7 @@
 <script lang="ts">
 	import type { UINode } from '$lib/types';
-	import ActionPanelSection from './nodes/ActionPanelSection.svelte';
-	import ActionCopyToClipboard from './nodes/actions/CopyToClipboard.svelte';
-	import ActionOpenInBrowser from './nodes/actions/OpenInBrowser.svelte';
-	import ActionPanel from './nodes/ActionPanel.svelte';
-	import Action from './nodes/actions/Action.svelte';
-	import ActionPush from './nodes/actions/ActionPush.svelte';
-	import Detail from './nodes/detail/Detail.svelte';
-	import Metadata from './nodes/detail/Metadata.svelte';
-	import MetadataLabel from './nodes/detail/MetadataLabel.svelte';
-	import MetadataLink from './nodes/detail/MetadataLink.svelte';
-	import MetadataSeparator from './nodes/detail/MetadataSeparator.svelte';
-	import MetadataTagList from './nodes/detail/MetadataTagList.svelte';
-	import MetadataTagListItem from './nodes/detail/MetadataTagListItem.svelte';
-	import GridDropdown from './nodes/grid/Dropdown.svelte';
-	import GridDropdownItem from './nodes/grid/DropdownItem.svelte';
-	import GridDropdownSection from './nodes/grid/DropdownSection.svelte';
+	import { componentMap } from '$lib/nodes';
 
-	// TODO: maybe make uiTree global
 	type Props = {
 		nodeId: number;
 		uiTree: Map<number, UINode>;
@@ -26,42 +10,13 @@
 		primaryActionNodeId?: number;
 	};
 
-	let { nodeId, uiTree, onDispatch, displayAs = 'item', primaryActionNodeId }: Props = $props();
+	let { nodeId, uiTree, onDispatch, ...restProps }: Props = $props();
 	const node = $derived(uiTree.get(nodeId));
+	const Component = $derived(node ? componentMap.get(node.type) : null);
 </script>
 
-{#if node}
-	{#if node.type === 'Action'}
-		<Action {nodeId} {uiTree} {onDispatch} {displayAs} />
-	{:else if node.type === 'Action.Panel'}
-		<ActionPanel {nodeId} {uiTree} {onDispatch} {primaryActionNodeId} />
-	{:else if node.type === 'Action.Panel.Section'}
-		<ActionPanelSection {nodeId} {uiTree} {onDispatch} />
-	{:else if node.type === 'Action.CopyToClipboard'}
-		<ActionCopyToClipboard {nodeId} {uiTree} {onDispatch} {displayAs} />
-	{:else if node.type === 'Action.OpenInBrowser'}
-		<ActionOpenInBrowser {nodeId} {uiTree} {onDispatch} {displayAs} />
-	{:else if node.type === 'Action.Push'}
-		<ActionPush {nodeId} {uiTree} {onDispatch} {displayAs} />
-	{:else if node.type === 'Detail'}
-		<Detail {nodeId} {uiTree} {onDispatch} />
-	{:else if node.type === 'Detail.Metadata'}
-		<Metadata {nodeId} {uiTree} {onDispatch} />
-	{:else if node.type === 'Detail.Metadata.Label'}
-		<MetadataLabel {nodeId} {uiTree} {onDispatch} />
-	{:else if node.type === 'Detail.Metadata.Link'}
-		<MetadataLink {nodeId} {uiTree} {onDispatch} />
-	{:else if node.type === 'Detail.Metadata.TagList'}
-		<MetadataTagList {nodeId} {uiTree} {onDispatch} />
-	{:else if node.type === 'Detail.Metadata.TagList.Item'}
-		<MetadataTagListItem {nodeId} {uiTree} {onDispatch} />
-	{:else if node.type === 'Detail.Metadata.Separator'}
-		<MetadataSeparator {nodeId} {uiTree} {onDispatch} />
-	{:else if node.type === 'Grid.Dropdown'}
-		<GridDropdown {nodeId} {uiTree} {onDispatch} />
-	{:else if node.type === 'Grid.Dropdown.Section'}
-		<GridDropdownSection {nodeId} {uiTree} {onDispatch} />
-	{:else if node.type === 'Grid.Dropdown.Item'}
-		<GridDropdownItem {nodeId} {uiTree} {onDispatch} />
-	{/if}
+{#if node && Component}
+	<Component {nodeId} {uiTree} {onDispatch} {...restProps} />
+{:else if node}
+	<div class="p-2 text-xs text-red-500">Unknown component type: {node.type}</div>
 {/if}
---
+++ src/lib/nodes.ts
@@ -0,0 +1,44 @@
+import type { Component } from 'svelte';
+import type { UINode } from './types';
+import Action from '$lib/components/nodes/actions/Action.svelte';
+import ActionPanel from '$lib/components/nodes/ActionPanel.svelte';
+import ActionPanelSection from '$lib/components/nodes/ActionPanelSection.svelte';
+import ActionCopyToClipboard from '$lib/components/nodes/actions/CopyToClipboard.svelte';
+import ActionOpenInBrowser from '$lib/components/nodes/actions/OpenInBrowser.svelte';
+import ActionPush from '$lib/components/nodes/actions/ActionPush.svelte';
+import Detail from '$lib/components/nodes/detail/Detail.svelte';
+import Metadata from '$lib/components/nodes/detail/Metadata.svelte';
+import MetadataLabel from '$lib/components/nodes/detail/MetadataLabel.svelte';
+import MetadataLink from '$lib/components/nodes/detail/MetadataLink.svelte';
+import MetadataTagList from '$lib/components/nodes/detail/MetadataTagList.svelte';
+import MetadataTagListItem from '$lib/components/nodes/detail/MetadataTagListItem.svelte';
+import MetadataSeparator from '$lib/components/nodes/detail/MetadataSeparator.svelte';
+import GridDropdown from '$lib/components/nodes/grid/Dropdown.svelte';
+import GridDropdownSection from '$lib/components/nodes/grid/DropdownSection.svelte';
+import GridDropdownItem from '$lib/components/nodes/grid/DropdownItem.svelte';
+
+export const componentMap = new Map<
+	string,
+	Component<{
+		nodeId: number;
+		uiTree: Map<number, UINode>;
+		onDispatch: (instanceId: number, handlerName: string, args: unknown[]) => void;
+	}>
+>([
+	['Action', Action],
+	['Action.Panel', ActionPanel],
+	['Action.Panel.Section', ActionPanelSection],
+	['Action.CopyToClipboard', ActionCopyToClipboard],
+	['Action.OpenInBrowser', ActionOpenInBrowser],
+	['Action.Push', ActionPush],
+	['Detail', Detail],
+	['Detail.Metadata', Metadata],
+	['Detail.Metadata.Label', MetadataLabel],
+	['Detail.Metadata.Link', MetadataLink],
+	['Detail.Metadata.TagList', MetadataTagList],
+	['Detail.Metadata.TagList.Item', MetadataTagListItem],
+	['Detail.Metadata.Separator', MetadataSeparator],
+	['Grid.Dropdown', GridDropdown],
+	['Grid.Dropdown.Section', GridDropdownSection],
+	['Grid.Dropdown.Item', GridDropdownItem]
+]);
